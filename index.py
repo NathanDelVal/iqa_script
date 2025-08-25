bd6c50103b0de8f0d651009e50d2cb9a2a36aaba
Nathan M <nathan.madeira2908@gmail.com>
2025-08-25 15:28:52 -0300

    add custom column logic

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import params
 from datetime import datetime
 import dateparser
 import argparse
+from unidecode import unidecode
 
 target_month = 8
 
@@ -24,8 +25,6 @@ conc_dict = {
  9: 'mai', 10: 'jun', 11: 'jul', 12: 'ago'
 }
 
-sample_iqa_sheets = pd.read_excel(params.example_file, sheet_name=["Dados_Conc", "Plan_Conc"])
-sample_iqa_columns = {x:y.columns.tolist() for x,y in sample_iqa_sheets.items()}
 blocos = {"a": params.bloco_a, "b": params.bloco_b, "c": params.bloco_c}
 
 iqa_sheets = {"Plan_Conc": [], "Dados_Conc": []}
@@ -37,21 +36,27 @@ if args.input_files:
             raise Exception(f"O arquivo {x} não está no padrão correto! Favor padronizar. Ex: ...(a).xlsx")
         try:
             df = pd.read_excel(x, sheet_name=sheet_reference_name)
-            lab_column = df[sheet_reference["Dados_Conc"]]["Laboratorio Analise"].values.tolist() if "Laboratorio Analise" in df[sheet_reference["Dados_Conc"]].columns.tolist() else None
-            tipo_column = df[sheet_reference["Dados_Conc"]]["TIPO"].values.tolist() if "TIPO" in df[sheet_reference["Dados_Conc"]].columns.tolist() else None
-            if lab_column is not None:
-                the_column = ["Interno" if re.search("RMM - LT - Bioagri", str(v), re.IGNORECASE) else "Externo" if not pd.isna(v) else v for v in lab_column]
-            if tipo_column is not None:
-                the_column = ["Interno" if re.search("externo", str(v), re.IGNORECASE) is None else "Externo" if not pd.isna(v) else v for v in tipo_column]
             for sheet in sheet_reference_name:
-                cols_to_drop = [col for col in df[sheet].columns if re.search("Unnamed", col)] + ["Concessão", "Empresa"]
-                df[sheet] = df[sheet].drop([col for col in cols_to_drop if col in df[sheet].columns], axis=1)
-                prepend = pd.DataFrame({
+                final_col = []
+                prepend = {
                     "Relatório": [f"{args.referencia}{''.join(re.findall(params.file_pattern, x)).upper()}"] * len(df[sheet]),
                     "Mês Ref": [f"{conc_dict[args.referencia]}/{datetime.now().__format__('%y')}"] * len(df[sheet]),
                     "Bloco": ["".join(re.findall(params.file_pattern, x)).upper()] * len(df[sheet]),
-                    "Empresa": [blocos["".join(re.findall(params.file_pattern, x)).lower()]] * len(df[sheet])        
-                })
+                    "Empresa": [blocos["".join(re.findall(params.file_pattern, x)).lower()]] * len(df[sheet])
+                }
+                if sheet == "08-RST_ANL_VRF":
+                    sheet_cols = df[sheet].columns.tolist()
+                    lab_col = "".join([x for x in sheet_cols if unidecode(x.lower().replace(" ","_")) == "laboratorio_analise"])
+                    tipo_col = "".join([x for x in sheet_cols if unidecode(x.lower().replace(" ","_")) == "tipo"])
+                    lab_col = df[sheet][lab_col].values.tolist() if lab_col in sheet_cols else None
+                    tipo_col = df[sheet][tipo_col].values.tolist() if tipo_col in sheet_cols else None
+                    if lab_col is not None:
+                        prepend["Tipo Laboratório"] = ["Interno" if re.search("RMM - LT - Bioagri", str(v), re.IGNORECASE) else "Externo" if not pd.isna(v) else v for v in lab_col]
+                    if tipo_col is not None:
+                        prepend["Tipo Laboratório"] = ["Interno" if re.search("externo", str(v), re.IGNORECASE) is None else "Externo" if not pd.isna(v) else v for v in tipo_col]
+                prepend = pd.DataFrame(prepend)        
+                cols_to_drop = [col for col in df[sheet].columns if re.search("Unnamed", col)] + ["Concessão", "Empresa"]
+                df[sheet] = df[sheet].drop([col for col in cols_to_drop if col in df[sheet].columns], axis=1)
                 iqa_sheets["".join([k for k, v in sheet_reference.items() if v == sheet])].append(pd.concat([prepend, df[sheet]], axis=1))
             print("✅ Excel file loaded successfully!")
         except FileNotFoundError:
@@ -61,9 +66,9 @@ else:
     raise Exception("Por favor, inclua os nomes dos arquivos nos argumentos! Ex: python index.py [arquivo1] [arquivo2]...")
 
 for k, v in iqa_sheets.items():
-    intersection_columns_list = list(set.intersection(*[set([col.lower().replace(" ","_") for col in df.columns]) for df in v]))
+    intersection_columns_list = list(set.intersection(*[set([unidecode(col.lower().replace(" ","_")) for col in df.columns]) for df in v]))
     for sheet in v:
-        result = [col for col in sheet.columns.to_list() if col.lower().replace(" ","_") in intersection_columns_list]
+        result = [col for col in sheet.columns.to_list() if unidecode(col.lower().replace(" ","_")) in intersection_columns_list]
         sheet = sheet[result]
     iqa_sheets[k] = pd.concat(v)
 
